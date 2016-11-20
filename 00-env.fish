4050ce33da8259c530413f0c245974533dd35aab
Dongsheng Cai <d@tux.im>
2016-11-20 16:28:12 +1100

    00-env.fish

---
+++ 00-env.fish
@@ -0,0 +1,7 @@
+set -gx SHELL /usr/local/bin/fish
+set -gx EDITOR vim
+set -gx LC_ALL en_US.UTF-8
+set -gx HTTPROOT "$HOME/src"
+set -gx TIME_STYLE long-iso
+set -gx PAGER less
+set -gx fish_prompt_pwd_dir_length 3
