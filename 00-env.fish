a5f94058076f89f5210571039ae0353db2c63527
Dongsheng Cai <d@tux.im>
2016-11-27 13:44:26 +1100

    git prompt signs

--- 00-env.fish
+++ 00-env.fish
@@ -5,3 +5,16 @@ set -gx HTTPROOT "$HOME/src"
 set -gx TIME_STYLE long-iso
 set -gx PAGER less
 set -gx fish_prompt_pwd_dir_length 3
+
+# fish git prompt
+set -gx __fish_git_prompt_showdirtystate 'yes'
+set -gx __fish_git_prompt_showstashstate 'yes'
+set -gx __fish_git_prompt_showupstream 'yes'
+set -gx __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set -gx __fish_git_prompt_char_dirtystate '⚡'
+set -gx __fish_git_prompt_char_stagedstate '→'
+set -gx __fish_git_prompt_char_stashstate '↩'
+set -gx __fish_git_prompt_char_upstream_ahead '↑'
+set -gx __fish_git_prompt_char_upstream_behind '↓'
