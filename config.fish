43cc413bb63bad24287cda006c9b873d4b8bfa73
Dongsheng Cai <d@tux.im>
2017-02-07 15:35:26 +1100

    cleanup add to PATH code

--- config.fish
+++ config.fish
@@ -1,12 +1,12 @@
 set -x FISHHOME "$HOME/.config/fish"
-#set -U fish_path "$HOME/.local/var/fisherman"
 
 function source_script
   test -f $argv[1]; and source $argv[1]
 end
 
 function add_one_path --description 'Add a directory to $PATH'
-  test -d $argv[1]; and set PATH $argv[1] $PATH
+  #test -d $argv[1]; and set PATH $argv[1] $PATH
+  test -d $argv[1]; and set -U fish_user_paths $argv[1] $fish_user_paths
 end
 
 function add_paths --description 'Add a list of paths to $PATH'
@@ -15,22 +15,24 @@ function add_paths --description 'Add a list of paths to $PATH'
   end
 end
 
+source_script $FISHHOME/iterm2.fish;
 source_script $FISHHOME/00-env.fish
 source_script $FISHHOME/20-local.fish
 source_script $FISHHOME/80-credentials.fish
+source_script $FISHHOME/learnosity.fish;
 
 add_paths \
   /usr/local/share/npm/bin \
   /usr/local/sbin \
   /usr/local/bin \
-  ~/.cabal/bin \
   ~/Dropbox/bin \
+  ~/.cabal/bin \
   ~/.npm-packages/bin \
   ~/.composer/vendor/bin \
-  ~/Library/Python/3.5/bin \
-  "$GOPATH/bin" \
   ~/.phpenv/bin \
-  ~/Library/Python/2.7/bin
+  ~/Library/Python/3.5/bin \
+  ~/Library/Python/2.7/bin \
+  "$GOPATH/bin"
 
 if type --quiet "brew"
   add_one_path ""(brew --prefix)"/bin"
@@ -39,6 +41,3 @@ end
 if type --quiet "ruby"
   add_one_path ""(ruby -rubygems -e 'puts Gem.user_dir')"/bin"
 end
-
-source_script $FISHHOME/learnosity.fish;
-source_script $FISHHOME/iterm2.fish;
