978d05a74eb5e74bb72d6f82871aaaaf82619cfd
Dongsheng Cai <d@tux.im>
2017-12-27 15:30:46 +1100

    align fish shell vars

--- conf.d/00-env.fish
+++ conf.d/00-env.fish
@@ -1,27 +1,25 @@
-set -gx SHELL (which fish)
-set -gx EDITOR vim
-set -gx LC_ALL en_US.UTF-8
-set -gx HTTPROOT "$HOME/src"
-set -gx TIME_STYLE long-iso
-set -gx PAGER less
-set -gx fish_prompt_pwd_dir_length 3
-set -gx GOPATH "$HOME/.go"
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -gx LESSOPEN '|pygmentize %s'
-
+set -gx SHELL                                  (which fish)
+set -gx EDITOR                                 vim
+set -gx LC_ALL                                 en_US.UTF-8
+set -gx GOPATH                                 "$HOME/.go"
+set -gx XDG_CACHE_HOME                         "$HOME/.cache"
+set -gx XDG_CONFIG_HOME                        "$HOME/.config"
+set -gx XDG_DATA_HOME                          "$HOME/.local/share"
+set -gx HTTPROOT                               "$HOME/src"
+set -gx TIME_STYLE                             long-iso
+set -gx PAGER                                  less
+set -gx fish_prompt_pwd_dir_length             3
+set -gx LESSOPEN                               '|pygmentize %s'
 # fish git prompt
-set -gx __fish_git_prompt_showdirtystate 'yes'
-set -gx __fish_git_prompt_showstashstate 'yes'
-set -gx __fish_git_prompt_showupstream 'yes'
-set -gx __fish_git_prompt_color_branch yellow
-
+set -gx __fish_git_prompt_showdirtystate       'yes'
+set -gx __fish_git_prompt_showstashstate       'yes'
+set -gx __fish_git_prompt_showupstream         'yes'
+set -gx __fish_git_prompt_color_branch         yellow
 # Status Chars
-set -gx __fish_git_prompt_char_dirtystate '⚡'
-set -gx __fish_git_prompt_char_stagedstate '→'
-set -gx __fish_git_prompt_char_stashstate '↩'
-set -gx __fish_git_prompt_char_upstream_ahead '↑'
+set -gx __fish_git_prompt_char_dirtystate      '⚡'
+set -gx __fish_git_prompt_char_stagedstate     '→'
+set -gx __fish_git_prompt_char_stashstate      '↩'
+set -gx __fish_git_prompt_char_upstream_ahead  '↑'
 set -gx __fish_git_prompt_char_upstream_behind '↓'
 
 function source_script
