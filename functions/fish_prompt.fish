edcbf3ecbace71da4d3cc8e89e9f54c80620d30b
Dongsheng Cai <d@tux.im>
2018-05-31 23:45:25 +1000

    change fish prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,32 +1,9 @@
-function fish_prompt --description 'Write out the prompt'
-    # Just calculate this once, to save a few cycles when displaying the prompt
-    if not set -q __fish_prompt_hostname
-        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-    end
+function fish_prompt
+	test $SSH_TTY
+    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+    test "$USER" = 'root'
+    and echo (set_color red)"#"
 
-    set -l color_cwd
-    set -l suffix
-    switch $USER
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
-    end
-
-    set match_start (set_color blue)'('(set_color normal)
-    set match_end (set_color blue)')'(set_color normal)
-
-    echo -n -s \
-      "$match_start" \
-      "$USER" @ "$__fish_prompt_hostname" \
-      "$match_end" \
-      ' ' \
-      (set_color $color_cwd) (prompt_pwd) (set_color normal) \
-      "$suffix "
+    # Main
+    echo -n (set_color cyan)(prompt_pwd) (set_color green)'❯ '
 end
