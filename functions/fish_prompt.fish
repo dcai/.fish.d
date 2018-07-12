81f75a9327e4e692d60b0072a027f602fc120983
Dongsheng Cai <d@tux.im>
2018-07-12 14:17:35 +0000

    Improved fish prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,10 +1,11 @@
 function fish_prompt
   # test $SSH_TTY
   # and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
-  test "$USER" = 'root'
-  and echo (set_color red)"#"
-
-  # Main
-  echo -n (set_color cyan)(prompt_pwd) (set_color green)'> '
+  switch "$USER"
+    case root
+      echo -n (set_color red)(prompt_pwd) "# "
+    case '*'
+      echo -n (set_color cyan)(prompt_pwd) (set_color green)'> '
+  end
   set_color normal
 end
