257e4b0e230bcd6fa7b3f50c0d365cc1fa9b5374
Dongsheng Cai <d@tux.im>
2020-01-23 22:04:08 +0800

    keybindings: c-o to list oneliners

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,4 +1,14 @@
+function fzf-oneliners-widget -d "Show oneliners"
+  set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
+  begin
+    set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT $FZF_DEFAULT_OPTS"
+    # (commandline) read current line content
+    one | eval fzf -q '(commandline)' | read -l result
+    and commandline -- $result
+  end
+  commandline -f repaint
+end
 function fish_user_key_bindings
   fzf_key_bindings
-  bind \co 'one |  read -lz result; and commandline -- $result; commandline -f repaint'
+  bind \co fzf-oneliners-widget
 end
