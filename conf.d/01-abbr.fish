29144089020adf9d71b959390504cc90776714d8
Dongsheng Cai <d@tux.im>
2020-09-16 10:04:57 +1000

    abbr: python watch tests

--- conf.d/01-abbr.fish
+++ conf.d/01-abbr.fish
@@ -2,4 +2,5 @@ if status --is-interactive
   abbr --add --global first 'echo my first abbreviation'
   abbr --add --global second 'echo my second abbreviation'
   abbr --add --global gco git checkout
+  abbr --add --global pywatch 'while true; find . -iname "*.py" | entr -rdc poetry run test; end'
 end
