413cb3efdcb9d179ca5de43f0c76daea07fc9480
Dongsheng Cai <d@tux.im>
2020-10-14 09:07:51 +1100

    macos: add python 3.9 path

--- dialect/macos.fish
+++ dialect/macos.fish
@@ -4,4 +4,5 @@ add_paths \
   ~/Library/Python/2.7/bin \
   ~/Library/Python/3.6/bin \
   ~/Library/Python/3.7/bin \
-  ~/Library/Python/3.8/bin
+  ~/Library/Python/3.8/bin \
+  ~/Library/Python/3.9/bin
