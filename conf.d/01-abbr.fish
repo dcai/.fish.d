1d11babf1e29a0d1ad1b15d62c45c6c2cc168acc
Dongsheng Cai <d@tux.im>
2020-12-26 20:49:21 +0800

    abbr: add dfc and vd

--- conf.d/01-abbr.fish
+++ conf.d/01-abbr.fish
@@ -1,6 +1,6 @@
 if status --is-interactive
-  abbr --add --global first 'echo my first abbreviation'
-  abbr --add --global second 'echo my second abbreviation'
   abbr --add --global gco git checkout
   abbr --add --global pywatch 'while true; find . -iname "*.py" | entr -rdc poetry run test; end'
+  abbr --add --global dfc 'watch -c -n 30 dfc -c always'
+  abbr --add --global vd "env HTTPS_PROXY="http://127.0.0.1:1087/" annie -aria2"
 end
