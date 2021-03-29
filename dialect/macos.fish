4cf8b53314547600c9121ebee4c6c4212d0603e3
Dongsheng Cai <d@tux.im>
2021-03-29 21:36:54 +1100

    mysql-client path

--- dialect/macos.fish
+++ dialect/macos.fish
@@ -2,6 +2,7 @@ set -gx HOMEBREW_NO_AUTO_UPDATE 1
 
 add_paths \
   /usr/local/opt/mysql-client/bin \
+  /opt/homebrew/opt/mysql-client/bin \
   ~/Library/Python/2.7/bin \
   ~/Library/Python/3.6/bin \
   ~/Library/Python/3.7/bin \
