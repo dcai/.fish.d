363337df26a9b237c50aead1b8d51e32e4e0f190
Dongsheng Cai <d@tux.im>
2021-05-30 21:52:12 +1000

    gcp: add env

--- conf.d/26-gcloud.fish
+++ conf.d/26-gcloud.fish
@@ -1,1 +1,6 @@
 if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path.fish.inc"; end
+
+
+set -gx GOOGLE_REGION 'australia-southeast1'
+set -gx GOOGLE_PROJECT 'foleo'
+set -gx GOOGLE_CREDENTIALS "$HOME/serviceaccount.$GOOGLE_PROJECT.json"
