c6f2839ef02d1b1389d4339d0f5efef3bfb79dcf
Dongsheng Cai <d@tux.im>
2021-05-31 11:43:43 +1000

    gcloud

--- conf.d/26-gcloud.fish
+++ conf.d/26-gcloud.fish
@@ -2,5 +2,6 @@ if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path
 
 
 set -gx GOOGLE_REGION 'australia-southeast1'
+set -gx GOOGLE_ZONE 'australia-southeast1-a'
 set -gx GOOGLE_PROJECT 'foleo'
 set -gx GOOGLE_CREDENTIALS "$HOME/serviceaccount.$GOOGLE_PROJECT.json"
