0b6d5b857384147a6e974af10e27e5f9cf76e0cc
Dongsheng Cai <d@tux.im>
2021-06-01 00:05:50 +1000

    abbr

--- conf.d/01-abbr.fish
+++ conf.d/01-abbr.fish
@@ -1,12 +1,12 @@
 if status --is-interactive
-    abbr --add --global dfc 'watch -c -n 30 dfc -c always'
-    abbr --add --global ld "tree --dirsfirst -aFCNL 1"
-    abbr --add --global lld "tree --dirsfirst -ChFupDaLg 1"
-    abbr --add --global mux 'tmuxp load'
-    abbr --add --global pywatch 'while true; find . -iname "*.py" | entr -rdc poetry run test; end'
-    abbr --add --global vd "env HTTPS_PROXY='http://127.0.0.1:1087/' annie -aria2"
-    abbr --add --global vim-upgrade "vim +PlugUpdate +qall"
-    abbr --add --global wget "curl -C - -O"
-    abbr --add --global behat-init-a11y "php admin/tool/behat/cli/init.php -j=1 --axe -a=classic"
-    abbr --add --global behat-run-a11y 'ROOT="$HOME/moodles/stable_master" vendor/bin/behat --format="moodle_progress" --out="std" --format="progress" --out="$ROOT/extra/behat/progress.txt" --format="pretty" --out="$ROOT/extra/behat/progress_pretty.txt" --config="$ROOT/moodledata_behat/behatrun/behat/behat.yml" --profile=headlesschrome --tags "@accessibility"'
+    abbr -a -g dfc 'watch -c -n 30 dfc -c always'
+    abbr -a -g ld 'tree --dirsfirst -aFCNL 1'
+    abbr -a -g lld 'tree --dirsfirst -ChFupDaLg 1'
+    abbr -a -g mux 'tmuxp load'
+    abbr -a -g pywatch 'while true; find . -iname "*.py" | entr -rdc poetry run test; end'
+    abbr -a -g vd "env HTTPS_PROXY='http://127.0.0.1:1087/' annie -aria2"
+    abbr -a -g vim-upgrade "vim +PlugUpdate +qall"
+    abbr -a -g wget "curl -C - -O"
+    abbr -a -g behat-init-a11y "php admin/tool/behat/cli/init.php -j=1 --axe -a=classic"
+    abbr -a -g behat-run-a11y 'ROOT="$HOME/moodles/stable_master" vendor/bin/behat --format="moodle_progress" --out="std" --format="progress" --out="$ROOT/extra/behat/progress.txt" --format="pretty" --out="$ROOT/extra/behat/progress_pretty.txt" --config="$ROOT/moodledata_behat/behatrun/behat/behat.yml" --profile=headlesschrome --tags "@accessibility"'
 end
