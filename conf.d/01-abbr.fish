f1d333b488e7496ab0213cfdd4036e9546626959
Dongsheng Cai <d@tux.im>
2021-08-04 20:13:17 +1000

    behat

--- conf.d/01-abbr.fish
+++ conf.d/01-abbr.fish
@@ -8,6 +8,6 @@ if status --is-interactive
     abbr -a -g vd "env HTTPS_PROXY='http://127.0.0.1:1087/' annie -aria2"
     abbr -a -g vim-upgrade "vim +PlugUpdate +qall"
     abbr -a -g wget "curl -C - -O"
-    abbr -a -g behat-init-a11y "php admin/tool/behat/cli/init.php -j=1 --axe -a=classic"
-    abbr -a -g behat-run-a11y 'ROOT="$HOME/moodles/stable_master" vendor/bin/behat --format="moodle_progress" --out="std" --format="progress" --out="$ROOT/extra/behat/progress.txt" --format="pretty" --out="$ROOT/extra/behat/progress_pretty.txt" --config="$ROOT/moodledata_behat/behatrun/behat/behat.yml" --profile=headlesschrome --tags "@accessibility"'
+    abbr -a -g behat-init "php admin/tool/behat/cli/init.php -j=1 --axe -a=classic"
+    abbr -a -g behat-run 'ROOT="$HOME/moodles/stable_master" vendor/bin/behat --format="moodle_progress" --out="std" --format="progress" --out="$ROOT/extra/behat/progress.txt" --format="pretty" --out="$ROOT/extra/behat/progress_pretty.txt" --config="$ROOT/moodledata_behat/behatrun/behat/behat.yml" --profile=headlessfirefox --tags "@accessibility"'
 end
