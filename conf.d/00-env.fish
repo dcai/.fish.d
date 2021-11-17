1297a50755c9847b4b0bb2bc84ff0c788a3e93bf
DongSheng Cai <DongSheng.Cai@iag.com.au>
2021-11-18 09:45:11 +1100

    iprintf

--- conf.d/00-env.fish
+++ conf.d/00-env.fish
@@ -39,9 +39,9 @@ function add_paths --description 'Add a list of paths to $PATH'
     end
 end
 
-function echoi --description "echo in interactive mode only"
+function iprintf --description "echo in interactive mode only"
     if status --is-interactive
-        echo $argv[1]
+        printf $argv[1]
     end
 end
 
